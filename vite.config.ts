c37a714c7f88f1ad99cdad6659089c07b60f0f4e
sardor01 <sardor.astanov.01@mail.ru>
2024-09-10 23:22:29 +0500

    fix: dist generation

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
